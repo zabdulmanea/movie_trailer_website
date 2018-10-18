34bdf58dd3cdcf00c1736fd457739754c1781809
Zainab Abdulmanea <35572741+zabdulmanea@users.noreply.github.com>
2018-10-18 22:34:05 +0300

    Improve URL code quality

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,9 +5,7 @@ import fresh_tomatoes
 # Instance information: (Title, image poster URL, Youtube URL)
 distorted = media.Movie(
     "Distorted 2018",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/"
-    "_640x_3aba8138970853f8032b0fc5aba951f4b0e3a4f48fa7"
-    "ecc5373843f7e9e4b947.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_640x_3aba8138970853f8032b0fc5aba951f4b0e3a4f48fa7ecc5373843f7e9e4b947.jpg",  # NOQA
     "https://www.youtube.com/watch?v=SAIaJOIx_2M")
 
 game_night = media.Movie(
@@ -22,22 +20,17 @@ isle_of_dogs = media.Movie(
 
 monster_calls = media.Movie(
     " A Monster Calls 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/"
-    "_640x_c8ceb001295ec477349beb61adf5f8d8c5d026f46711bb"
-    "ef17d02027c9213a87.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_640x_c8ceb001295ec477349beb61adf5f8d8c5d026f46711bbef17d02027c9213a87.jpg",  # NOQA
     "https://www.youtube.com/watch?v=R2Xbo-irtBA")
 
 arrival = media.Movie(
     "Arrival 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/"
-    "_640x_5a50a917eec6d52adc2c7e420d1d591ca18d7d7b839102"
-    "159403dc8860faa339.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_640x_5a50a917eec6d52adc2c7e420d1d591ca18d7d7b839102159403dc8860faa339.jpg",  # NOQA
     "https://www.youtube.com/watch?v=AMgyWT075KY")
 
 three_billboards = media.Movie(
     "THREE BILLBOARDS OUTSIDE EBBING, MISSOURI 2017",
-    "https://upload.wikimedia.org/wikipedia/ar/8/86/"
-    "Three_billboards_outside_ebbing_missouri_xlg.jpg",
+    "https://upload.wikimedia.org/wikipedia/ar/8/86/Three_billboards_outside_ebbing_missouri_xlg.jpg",  # NOQA
     "https://www.youtube.com/watch?v=Jit3YhGx5pU")
 
 silent_voice = media.Movie(
@@ -47,57 +40,47 @@ silent_voice = media.Movie(
 
 all_the_money = media.Movie(
     "All the Money in the World 2017",
-    "https://upload.wikimedia.org/wikipedia/ar/f/"
-    "f0/All_the_Money_in_the_World.png",
+    "https://upload.wikimedia.org/wikipedia/ar/f/f0/All_the_Money_in_the_World.png",  # NOQA
     "https://www.youtube.com/watch?v=KXHrCBkIxQQ")
 
 beauty_and_the_beast = media.Movie(
     "Beauty and the Beast 2017",
-    "https://upload.wikimedia.org/wikipedia/en/d/d6/"
-    "Beauty_and_the_Beast_2017_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/d/d6/Beauty_and_the_Beast_2017_poster.jpg",  # NOQA
     "https://www.youtube.com/watch?v=OvW_L8sTu5E")
 
 dear_zindagi = media.Movie(
     "Dear Zindagi 2016",
-    "https://upload.wikimedia.org/wikipedia/ar/1/19/"
-    "%D9%85%D9%84%D8%B5%D9%82_%D8%AF%D9%8A%D8%B1_%D8%"
-    "B2%D9%8A%D9%86%D8%AF%D8%A7%D8%AC%D9%8A.jpg",
+    "https://upload.wikimedia.org/wikipedia/ar/1/19/%D9%85%D9%84%D8%B5%D9%82_%D8%AF%D9%8A%D8%B1_%D8%B2%D9%8A%D9%86%D8%AF%D8%A7%D8%AC%D9%8A.jpg",  # NOQA
     "https://www.youtube.com/watch?v=5DkO7ksXY8E")
 
 ethel_and_ernest = media.Movie(
     "Ethel & Ernest (2016)",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_446a76f838948"
-    "f3082a883306f8d2c1165f20ae4b403b2c6e6f9b6f22748d257.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_446a76f838948f3082a883306f8d2c1165f20ae4b403b2c6e6f9b6f22748d257.jpg",  # NOQA
     "https://www.youtube.com/watch?v=JXDlEw5u8u8")
 
 fantastic_beasts = media.Movie(
     "Fantastic Beasts and Where to Find Them 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_b5f12b4"
-    "273184d7a5990d33367c1c8a11e7b8d5f292d4a401a5ec624d1e51a5d.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_b5f12b4273184d7a5990d33367c1c8a11e7b8d5f292d4a401a5ec624d1e51a5d.jpg",  # NOQA
     "https://www.youtube.com/watch?v=ViuDsy7yb8M")
 
 goodbye_christopher = media.Movie(
     "Goodbye Christopher Robin 2017",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_6567e438d"
-    "3cd94c7898e315d0937f531363fea4959db57b11aed3240c41c0daf.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_6567e438d3cd94c7898e315d0937f531363fea4959db57b11aed3240c41c0daf.jpg",  # NOQA
     "https://www.youtube.com/watch?v=uQq3aFSijRg")
 
 jumanji3 = media.Movie(
     "Jumanji- Welcome to the Jungle 2017",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_5c46a"
-    "8003fd93d7ece1524dc3d3ddfabb1e811cf4e460d076ccb3e76462afd66.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_5c46a8003fd93d7ece1524dc3d3ddfabb1e811cf4e460d076ccb3e76462afd66.jpg",  # NOQA
     "https://www.youtube.com/watch?v=leE10vdvkho")
 
 lady_macbeth = media.Movie(
     "Lady Macbeth 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_c786ea7"
-    "4aef2647b124112e099d3d2c3be1e9354fb7b209a9aaf6db0e2648bd0.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_c786ea74aef2647b124112e099d3d2c3be1e9354fb7b209a9aaf6db0e2648bd0.jpg",  # NOQA
     "https://www.youtube.com/watch?v=Cpk1Gd2Red0")
 
 life = media.Movie(
     "Life 2017",
-    "https://upload.wikimedia.org/wikipedia/ar/1/13/%D9%85%D9%84%D8%B5%D9%82"
-    "_%D9%81%D9%8A%D9%84%D9%85_%D8%AD%D9%8A%D8%A7%D8%A9_%282017%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/ar/1/13/%D9%85%D9%84%D8%B5%D9%82_%D9%81%D9%8A%D9%84%D9%85_%D8%AD%D9%8A%D8%A7%D8%A9_%282017%29.jpg",  # NOQA
     "https://www.youtube.com/watch?v=dgOGqWHtjP0")
 
 little_forest = media.Movie(
@@ -117,58 +100,47 @@ manchester_by_the_sea = media.Movie(
 
 moonlight = media.Movie(
     "Moonlight (2016)",
-    "https://upload.wikimedia.org/wikipedia/ar/8/84/"
-    "Moonlight_%282016_film%29.png",
+    "https://upload.wikimedia.org/wikipedia/ar/8/84/Moonlight_%282016_film%29.png",  # NOQA
     "https://www.youtube.com/watch?v=9NJj12tJzqc")
 
 nocturnal_animals = media.Movie(
     "Nocturnal Animals 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_1a4b1f217e"
-    "52401f10e5e61066127e9031bd97bc354bf3354a7f1e328de16c29.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_1a4b1f217e52401f10e5e61066127e9031bd97bc354bf3354a7f1e328de16c29.jpg",  # NOQA
     "https://www.youtube.com/watch?v=-H1Ii1LjyFU")
 
 patriots_day = media.Movie(
     "Patriots Day (2017)",
-    "https://upload.wikimedia.org/wikipedia/ar/thumb/f/f7/Patriots_Day_%28fil"
-    "m%29.png/208px-Patriots_Day_%28film%29.png",
+    "https://upload.wikimedia.org/wikipedia/ar/thumb/f/f7/Patriots_Day_%28film%29.png/208px-Patriots_Day_%28film%29.png",  # NOQA
     "https://www.youtube.com/watch?v=KJXqh2rDehg")
 
 silence = media.Movie(
     "Silence (2016)",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/"
-    "_315x420_4f43815afb5d39d6"
-    "59c0d5a5401336e8bce2a7b91f43e17e4770eeba704194c8.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_4f43815afb5d39d659c0d5a5401336e8bce2a7b91f43e17e4770eeba704194c8.jpg",  # NOQA
     "https://www.youtube.com/watch?v=A0KUWzfugg4")
 
 edge_of_17 = media.Movie(
     "The Edge of Seventeen (2016)",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_d8c6bc1337"
-    "01bb4a57ca0aff31bd167fe1d3ecca3033b0837aef5a32eedded31.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_d8c6bc133701bb4a57ca0aff31bd167fe1d3ecca3033b0837aef5a32eedded31.jpg",  # NOQA
     "https://www.youtube.com/watch?v=EB6Gecy6IP8")
 
 salesman = media.Movie(
     "The Salesman 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_9b8de0"
-    "16e26280bc44de14f2142b661c6274661b345c0636b030993e9e9c8f6f.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_9b8de016e26280bc44de14f2142b661c6274661b345c0636b030993e9e9c8f6f.jpg",  # NOQA
     "https://www.youtube.com/watch?v=r-61yYjKHHc")
 
 underworld = media.Movie(
     "Underworld- Blood Wars 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_6f045a875"
-    "9afde88cdfda4ad5636c39603ee75952b932c090966da7e3736f6ba.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_6f045a8759afde88cdfda4ad5636c39603ee75952b932c090966da7e3736f6ba.jpg",  # NOQA
     "https://www.youtube.com/watch?v=rKHL5PyAPzs")
 
 unrest = media.Movie(
     "Unrest 2017",
-    "https://www.faselhd.co/wp-content/uploads/2017/11/MV5BYjI2YzY3ODEtN"
-    "TRlOS00YTQ5LWJlNDItMDgzZDZhMjQzNzk3XkEyXkFqcGdeQXVyNzkwOTM5MDk@._V1_"
-    "SY1000_CR006771000_AL_-435x623.jpg",
+    "https://www.faselhd.co/wp-content/uploads/2017/11/MV5BYjI2YzY3ODEtNTRlOS00YTQ5LWJlNDItMDgzZDZhMjQzNzk3XkEyXkFqcGdeQXVyNzkwOTM5MDk@._V1_SY1000_CR006771000_AL_-435x623.jpg",  # NOQA
     "https://www.youtube.com/watch?v=r7pGnfDvlWg")
 
 wind_river = media.Movie(
     "Wind River (2017)",
-    "https://upload.wikimedia.org/wikipedia/ar/thumb/2/2e/Wind_River_%282017"
-    "_film%29.png/219px-Wind_River_%282017_film%29.png",
+    "https://upload.wikimedia.org/wikipedia/ar/thumb/2/2e/Wind_River_%282017_film%29.png/219px-Wind_River_%282017_film%29.png",  # NOQA
     "https://www.youtube.com/watch?v=s7WuKdVhrmA")
 
 bad_teacher = media.Movie(
@@ -178,8 +150,7 @@ bad_teacher = media.Movie(
 
 better_watch_out = media.Movie(
     "Better Watch Out 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_dc06258af169"
-    "c2918c895bcbb28ba6148bf3dc6cf19385a57c19b76702ac7493.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_dc06258af169c2918c895bcbb28ba6148bf3dc6cf19385a57c19b76702ac7493.jpg",  # NOQA
     "https://www.youtube.com/watch?v=1BDxAsaoM1Y")
 
 bridesmaids = media.Movie(
@@ -189,8 +160,7 @@ bridesmaids = media.Movie(
 
 ferdinand = media.Movie(
     "Ferdinand 2017",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_fecae5"
-    "add393bd41286f269ddda1cba98e805fdd553ac79f2f5ab2547f3e5510.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_fecae5add393bd41286f269ddda1cba98e805fdd553ac79f2f5ab2547f3e5510.jpg",  # NOQA
     "https://www.youtube.com/watch?v=U9R679lvLeU")
 
 gimme_shelter = media.Movie(
@@ -205,56 +175,47 @@ goodbye_single = media.Movie(
 
 im_not_shamed = media.Movie(
     "I'm Not Ashamed 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_9225e86b2e87"
-    "3f1497ffa8daee651616687a1b7c19952ad4c38f7eed630c91f3.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_9225e86b2e873f1497ffa8daee651616687a1b7c19952ad4c38f7eed630c91f3.jpg",  # NOQA
     "https://www.youtube.com/watch?v=RJJ2xphZjjA")
 
 mirror_mirror = media.Movie(
     "Mirror Mirror 2012",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_25484fd"
-    "6578d21d1b4cace65e986542856f490c9f568d40ba87e631f89cfdda0.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_25484fd6578d21d1b4cace65e986542856f490c9f568d40ba87e631f89cfdda0.jpg",  # NOQA
     "https://www.youtube.com/watch?v=ILDeA7AqA84")
 
 money_monster = media.Movie(
     "Money Monster 2016",
-    "https://upload.wikimedia.org/wikipedia/ar/thumb/f/f2/"
-    "Money_Monster_poster.png/223px-Money_Monster_poster.png",
+    "https://upload.wikimedia.org/wikipedia/ar/thumb/f/f2/Money_Monster_poster.png/223px-Money_Monster_poster.png",  # NOQA
     "https://www.youtube.com/watch?v=va-0o_xBVnU")
 
 pink = media.Movie(
     "Pink 2016 ",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_3b88b4"
-    "8f4d094964fec8ad67cf2aed26763af5f3b22ec47b0e3d659cfe7d653c.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_3b88b48f4d094964fec8ad67cf2aed26763af5f3b22ec47b0e3d659cfe7d653c.jpg",  # NOQA
     "https://www.youtube.com/watch?v=AL2TShb6fFs")
 
 storks = media.Movie(
     "Storks 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_ce88aede4d"
-    "9e4860e1b6199c5758c01dec562f62fa6f1e47e8107b71d9df31a9.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_ce88aede4d9e4860e1b6199c5758c01dec562f62fa6f1e47e8107b71d9df31a9.jpg",  # NOQA
     "https://www.youtube.com/watch?v=ZENmkJk9fBM")
 
 when_the_bough = media.Movie(
     "When the Bough Breaks 2016",
-    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_f191573c35"
-    "da3f4a13cff5d2712ad659bf781e31a99cacc12047be13f82cd68b.jpg",
+    "http://photo.elcinema.com.s3.amazonaws.com/uploads/_315x420_f191573c35da3f4a13cff5d2712ad659bf781e31a99cacc12047be13f82cd68b.jpg",  # NOQA
     "https://www.youtube.com/watch?v=Ws74Ie4fMc8")
 
 bharat = media.Movie(
     "Bharat Ane Nenu (2018)",
-    "https://i2.wp.com/media.fushaar.com/media/2018/07/a0hFV8G0ofyPgPE"
-    "jVcYrueuxFex.jpg?resize=329%2C495&ssl=1",
+    "https://i2.wp.com/media.fushaar.com/media/2018/07/a0hFV8G0ofyPgPEjVcYrueuxFex.jpg?resize=329%2C495&ssl=1",  # NOQA
     "https://www.youtube.com/watch?v=orkPrGSAETs")
 
 hush = media.Movie(
     "Hush (2016)",
-    "https://i2.wp.com/media.fushaar.com/media/2016/06/"
-    "khDYdmPFlaTqeLQVD4ds4xC1mIf.jpg?resize=329%2C495&ssl=1",
+    "https://i2.wp.com/media.fushaar.com/media/2016/06/khDYdmPFlaTqeLQVD4ds4xC1mIf.jpg?resize=329%2C495&ssl=1",  # NOQA
     "https://www.youtube.com/watch?v=Q_P8WCbhC6s")
 
 house_by_lake = media.Movie(
     "House by the Lake 2017",
-    "https://m.media-amazon.com/images/M/MV5BMTMwMmM3N2UtMWE4Yy00NjU3LWI5ZT"
-    "gtNWNhOTc3OWY0MTNlXkEyXkFqcGdeQXVyMjQ4MTAyNTQ@._V1_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMTMwMmM3N2UtMWE4Yy00NjU3LWI5ZTgtNWNhOTc3OWY0MTNlXkEyXkFqcGdeQXVyMjQ4MTAyNTQ@._V1_.jpg",  # NOQA
     "https://www.youtube.com/watch?v=AU4PNrZPvYA")
 
 # create a list that contains favorite movies instances
@@ -270,5 +231,5 @@ movies = [
     mirror_mirror, money_monster, pink
 ]
 
-# call a method that creates a web page by passing movies list
+# call a method that creates html web page and pass movies list
 fresh_tomatoes.open_movies_page(movies)
